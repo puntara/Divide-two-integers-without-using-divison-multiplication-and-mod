e84fd4c74aa6e4382fc9e653272c0caf317abf34
Tara Pun <pun.tara@outlook.com>
2020-02-02 13:25:33 -0800

    first commit

---
+++ DivTwoInt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DivTwoInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long d = -20; 
+            long div = 7;
+            // part 1
+            long sign = (d < 0) ^ (div < 0) ? -1 : 1;
+
+            // part 2
+            long divident = Math.Abs(d);
+            long divisor = Math.Abs(div);
+
+            // part 3
+            long reminder = 0;
+            while ( divident>=divisor)
+            {
+                divident= divident - divisor;
+                reminder++;
+            }
+            reminder = sign * reminder;
+            // part 3
+            Console.WriteLine(reminder);
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,18 @@
+Problem is to divide number without using division multiplication and mod operator.
+return the quotient. 
+
+SOLUTION
+part 1
+calcuate the sign
+if 1 of them is negetive it will give -1;
+if both of them are negative it will give +
+
+part 2
+gives the absolute value of number, which is positive number 
+
+part 3 
+test the value of divident with divisor, 
+while the divident is smaller than divisor will turminate. 
+
+part 4
+print the of while loop. which is the answer. 
